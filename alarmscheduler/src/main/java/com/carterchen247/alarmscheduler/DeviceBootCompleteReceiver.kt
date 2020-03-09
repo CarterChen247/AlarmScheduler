fe4ee2beb7647645d42b2eb25271d75d7875a052
CarterChen <carterchen247@gmail.com>
2020-03-10 00:25:35 +0800

    Add DeviceBootCompleteReceiver

---
+++ alarmscheduler/src/main/java/com/carterchen247/alarmscheduler/DeviceBootCompleteReceiver.kt
@@ -0,0 +1,13 @@
+package com.carterchen247.alarmscheduler
+
+import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class DeviceBootCompleteReceiver : BroadcastReceiver() {
+    @SuppressLint("UnsafeProtectedBroadcastReceiver")
+    override fun onReceive(context: Context, intent: Intent) {
+
+    }
+}
