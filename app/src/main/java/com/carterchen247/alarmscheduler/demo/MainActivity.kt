578308ad1d24a517d3bce75bc4c497f830d692bb
CarterChen <carterchen247@gmail.com>
2020-03-10 15:31:40 +0800

    Adjust demo app code. Schedule alarm by click button.

--- app/src/main/java/com/carterchen247/alarmscheduler/demo/MainActivity.kt
+++ app/src/main/java/com/carterchen247/alarmscheduler/demo/MainActivity.kt
@@ -4,25 +4,20 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.carterchen247.alarmscheduler.AlarmConfig
 import com.carterchen247.alarmscheduler.AlarmScheduler
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // schedule an alarm
-        val config = AlarmConfig(DemoAlarmTask.TYPE, System.currentTimeMillis() + 5000L).apply {
-            //            alarmId = 1
-            customData = Bundle()
+        btnSchedule.setOnClickListener {
+            // schedule an alarm
+            val config = AlarmConfig(DemoAlarmTask.TYPE, System.currentTimeMillis() + 5000L).apply {
+                //            alarmId = 1
+                customData = Bundle()
+            }
+            AlarmScheduler.schedule(config)
         }
-        AlarmScheduler.schedule(config)
-
-//        Handler().postDelayed(Runnable {
-//            AlarmScheduler.cancel(1)
-//        }, 2000L)
-//        Handler().postDelayed(Runnable {
-//            AlarmScheduler.cancel(1)
-//        }, 3000L)
     }
 }
