51523b054c1cbdad8a2f3aeab27e98b5af396c23
CarterChen <carterchen247@gmail.com>
2020-03-15 12:39:17 +0800

    [NOTED]Remove RestartSchedulerReceiver
    Since Application#onCreate will be triggered before onReceive method of receiver which filter the action of BOOT_COMPLETE, it is not crucial to provide such a class. Remove it temporarily and will be add it back if is needed in the future

--- alarmscheduler/src/main/java/com/carterchen247/alarmscheduler/receiver/RebootCompleteReceiver.kt
+++ alarmscheduler/src/main/java/com/carterchen247/alarmscheduler/receiver/RebootCompleteReceiver.kt
@@ -1,30 +1,14 @@
 package com.carterchen247.alarmscheduler.receiver
 
-import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import com.carterchen247.alarmscheduler.RescheduleAlarmService
-import com.carterchen247.alarmscheduler.logger.Logger
+
 
 class RebootCompleteReceiver : BroadcastReceiver() {
 
-    @SuppressLint("UnsafeProtectedBroadcastReceiver")
     override fun onReceive(context: Context, intent: Intent) {
-        restartScheduler(context)
         RescheduleAlarmService.startService(context)
-    }
-
-    private fun restartScheduler(context: Context) {
-        Logger.d("restartScheduler")
-        val intent = Intent(RestartSchedulerReceiver.ACTION)
-        intent.setPackage(context.packageName)
-
-        val resolveInfos = context.packageManager.queryBroadcastReceivers(intent, 0)
-        resolveInfos.forEach { info ->
-            val name = info?.activityInfo?.name ?: return
-            val receiver = Class.forName(name).newInstance() as RestartSchedulerReceiver
-            receiver.restartScheduler()
-        }
     }
 }
