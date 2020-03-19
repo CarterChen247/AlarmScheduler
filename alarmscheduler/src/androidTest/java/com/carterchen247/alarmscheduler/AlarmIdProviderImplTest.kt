1a61175ed398173761527b41d49969c6f0858082
CarterChen <carterchen247@gmail.com>
2020-03-19 21:44:20 +0800

    Extract AlarmIdProviderImpl from AlarmIdProvider. Make it able to be tested

---
+++ alarmscheduler/src/androidTest/java/com/carterchen247/alarmscheduler/AlarmIdProviderImplTest.kt
@@ -0,0 +1,54 @@
+package com.carterchen247.alarmscheduler
+
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+class AlarmIdProviderImplTest {
+
+    private lateinit var impl: AlarmIdProviderImpl
+
+    @Before
+    fun setUp() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        impl = AlarmIdProviderImpl(appContext)
+    }
+
+    @After
+    fun tearDown() {
+        impl.clear()
+    }
+
+    @Test
+    fun testWithAssignedId() {
+        val id = impl.generateId(1)
+        assertEquals(1, id)
+    }
+
+    @Test
+    fun testWithoutAssignedId() {
+        var id: Int = impl.generateId(AlarmIdProvider.AUTO_GENERATE)
+        assertEquals(1, id)
+
+        id = impl.generateId(AlarmIdProvider.AUTO_GENERATE)
+        assertEquals(2, id)
+
+        id = impl.generateId(AlarmIdProvider.AUTO_GENERATE)
+        assertEquals(3, id)
+    }
+
+    @Test
+    fun testWithAssignedIdThenWithoutAssignedId() {
+        var id: Int = impl.generateId(50)
+        assertEquals(50, id)
+
+        id = impl.generateId(AlarmIdProvider.AUTO_GENERATE)
+        assertEquals(51, id)
+
+        id = impl.generateId(AlarmIdProvider.AUTO_GENERATE)
+        assertEquals(52, id)
+    }
+}
+
--- alarmscheduler/src/main/java/com/carterchen247/alarmscheduler/AlarmIdProvider.kt
+++ alarmscheduler/src/main/java/com/carterchen247/alarmscheduler/AlarmIdProvider.kt
@@ -2,22 +2,13 @@ package com.carterchen247.alarmscheduler
 
 
 import android.content.Context
-import android.content.Context.MODE_PRIVATE
-import java.util.concurrent.atomic.AtomicInteger
 
-class AlarmIdProvider(context: Context) {
+internal class AlarmIdProvider(context: Context) {
 
-    private val sharedPreference = context.getSharedPreferences(PREF_NAME, MODE_PRIVATE)
-    private val id by lazy { AtomicInteger(sharedPreference.getInt(KEY_ALARM_ID, 0)) }
+    private val impl: AlarmIdProviderImpl by lazy { AlarmIdProviderImpl(context) }
 
     fun generateId(alarmId: Int): Int {
-        if (alarmId != AUTO_GENERATE) {
-            sharedPreference.edit().putInt(KEY_ALARM_ID, alarmId).apply()
-            return alarmId
-        }
-        val generatedId = id.incrementAndGet()
-        sharedPreference.edit().putInt(KEY_ALARM_ID, generatedId).apply()
-        return generatedId
+        return impl.generateId(alarmId)
     }
 
     companion object {
@@ -29,9 +20,6 @@ class AlarmIdProvider(context: Context) {
                 instance ?: AlarmIdProvider(context).also { instance = it }
             }
         }
-
-        private const val PREF_NAME = "pref_alarm_id"
-        private const val KEY_ALARM_ID = "KEY_ALARM_ID"
         const val AUTO_GENERATE = 0
     }
 }
