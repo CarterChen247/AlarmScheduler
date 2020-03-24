1fc93581b8c75941ff1201b1e8c9ea1da14f5889
CarterChen <carterchen247@gmail.com>
2020-03-24 23:26:46 +0800

    Make DataPayload.create() internal

--- alarmscheduler/src/main/java/com/carterchen247/alarmscheduler/model/DataPayload.kt
+++ alarmscheduler/src/main/java/com/carterchen247/alarmscheduler/model/DataPayload.kt
@@ -55,7 +55,7 @@ class DataPayload {
     }
 
     companion object {
-        fun create(bundle: Bundle?): DataPayload {
+        internal fun create(bundle: Bundle?): DataPayload {
             if (bundle == null) {
                 return DataPayload()
             }
