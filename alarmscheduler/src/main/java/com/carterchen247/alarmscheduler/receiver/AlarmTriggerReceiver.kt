54ba4f1e725d5b9826498d4978d45c52a5ab02b0
CarterChen <carterchen247@gmail.com>
2020-04-09 21:44:40 +0800

    Add more logs to help app debug

--- alarmscheduler/src/main/java/com/carterchen247/alarmscheduler/receiver/AlarmTriggerReceiver.kt
+++ alarmscheduler/src/main/java/com/carterchen247/alarmscheduler/receiver/AlarmTriggerReceiver.kt
@@ -11,9 +11,11 @@ import com.carterchen247.alarmscheduler.storage.AlarmStateRepository
 
 class AlarmTriggerReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
+        Logger.d("AlarmTriggerReceiver.onReceive")
         val alarmType = intent.getIntExtra(Constant.ALARM_TYPE, Constant.VALUE_NOT_ASSIGN)
         val alarmId = intent.getIntExtra(Constant.ALARM_ID, Constant.VALUE_NOT_ASSIGN)
         val bundle = intent.getBundleExtra(Constant.ALARM_CUSTOM_DATA)
+        Logger.d("AlarmTriggerReceiver.onReceive alarmType=$alarmType alarmId=$alarmId bundle=$bundle")
         if (alarmType == Constant.VALUE_NOT_ASSIGN || alarmId == Constant.VALUE_NOT_ASSIGN) {
             return
         }
@@ -23,8 +25,12 @@ class AlarmTriggerReceiver : BroadcastReceiver() {
             return
         }
         Logger.d("Creating AlarmTask. alarmType=$alarmType alarmId=$alarmId")
-        val alarmTask = alarmTaskFactory.createAlarmTask(alarmType)
-        alarmTask.onAlarmFires(alarmId, DataPayload.create(bundle))
+        try {
+            val alarmTask = alarmTaskFactory.createAlarmTask(alarmType)
+            alarmTask.onAlarmFires(alarmId, DataPayload.create(bundle))
+        } catch (throwable: Throwable) {
+            Logger.d("Failed creating AlarmTask. throwable=$throwable")
+        }
         AlarmStateRepository.getInstance(context).removeImmediately(alarmId)
     }
 }
